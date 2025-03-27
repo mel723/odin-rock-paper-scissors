5d5aa74a958c4dab570e1b751190bf2f3230f321
Melanie <183251709+mel723@users.noreply.github.com>
2025-03-27 20:14:29 +1100

    Add ability to play a single round

--- script.js
+++ script.js
@@ -17,4 +17,40 @@ function getHumanChoice() {
   return prompt("Rock, paper, or scissors?")
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log(`You tied! Both you and the computer chose ${humanChoice}`);
+  } else {
+    let hasWon = false;
+
+    if (
+      humanChoice === "rock" && computerChoice === "scissors" || 
+      humanChoice === "paper" && computerChoice === "rock" ||
+      humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+      hasWon = true;
+    }
+
+    let message;
+
+    if (hasWon) {
+      message = `You won! ${humanChoice} beats ${computerChoice}`;
+      humanScore++;
+    } else {
+      message = `You lost! ${computerChoice} beats ${humanChoice}`;
+      computerScore++;
+    }
+    
+    console.log(message);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
